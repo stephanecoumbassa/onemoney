010482dfb294f23877dd7a6e21c0328d84f6e6db
COUMBASSA CHRISTIAN <c_jcoumbassa@ocitnetad.ci>
2023-01-05 16:01:16 +0000

    refactor: add drawer2

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,12 +7,20 @@ import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
 import React from "react";
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
-const Header = ({color}: any) => {
-  const [openMenu, setOpenMenu] = React.useState(false)
+const Header = ({ color }: any) => {
+  const [openMenu, setOpenMenu] = React.useState(false);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar color={color} position="static">
@@ -23,7 +31,9 @@ const Header = ({color}: any) => {
             color="inherit"
             aria-label="open drawer"
             sx={{ mr: 2 }}
-            onClick={() => {setOpenMenu(true)}}
+            onClick={() => {
+              setOpenMenu(true);
+            }}
           >
             <MenuIcon />
           </IconButton>
@@ -77,14 +87,46 @@ const Header = ({color}: any) => {
         </Toolbar>
       </AppBar>
 
-      <React.Fragment key={'left'}>
+      <React.Fragment key={"left"}>
         {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
         <Drawer
-          anchor={'left'}
+          anchor={"left"}
           open={openMenu}
-          onClose={() => {setOpenMenu(false)}}
+          onClose={() => {
+            setOpenMenu(false);
+          }}
         >
-          draw
+          <Box
+          sx={{ width: 'auto' }}
+          role="presentation">
+            <List>
+              {["Inbox", "Starred", "Send email", "Drafts"].map(
+                (text, index) => (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  </ListItem>
+                )
+              )}
+            </List>
+            <Divider />
+            <List>
+              {["All mail", "Trash", "Spam"].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Drawer>
       </React.Fragment>
     </Box>
